a655af22c652b41304c69cc045fad4129685af9a
leanmcp <helloworldcmu@gmail.com>
2026-03-15 20:54:44 +0800

    Fix ts error

--- packages/acp/src/index.ts
+++ packages/acp/src/index.ts
@@ -9,7 +9,7 @@ import { createAgent } from './agent.js'
 const pkg = JSON.parse(await readFile(new URL('../package.json', import.meta.url), 'utf-8'))
 
 const input = Writable.toWeb(process.stdout)
-const output = Readable.toWeb(process.stdin) as ReadableStream<Uint8Array>
+const output = Readable.toWeb(process.stdin) as unknown as ReadableStream<Uint8Array>
 const stream = ndJsonStream(input, output)
 
 new AgentSideConnection(
